9ecb167f188f6b1ed30037357f049c174dd409eb
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-05-30 12:59:43 -0700

    Initial checkin of general color-config.el, component for changing font faces (mostly to get things to look better with zenburn)

---
+++ config/color-config.el
@@ -0,0 +1,12 @@
+(custom-set-faces
+ ;; ECB
+ '(ecb-default-highlight-face ((((class color) (background dark)) (:background "#5b5b5b"))))
+ '(ecb-method-face ((((class color) (background dark)) (:inherit ecb-default-highlight-face :background "magenta"))))
+ ;; Magit
+ '(magit-diff-add ((((class color) (background dark)) (:foreground "#8CD0D3"))))
+ '(magit-diff-del ((((class color) (background dark)) (:foreground "#cc9393"))))
+ '(magit-item-highlight ((((class color) (background dark)) (:background "#4f4f4f"))))
+ '(magit-log-graph ((((class color) (background dark)) (:foreground "light green"))))
+ ;; MuMaMo mode
+ '(mmm-default-submode-face ((t (:background "#4f4f4f"))))
+ )
