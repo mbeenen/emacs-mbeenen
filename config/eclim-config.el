bec0c9cebf8cbf5bf42992b29465e92638442754
Michael Beenen <michael.beenen@gmail.com>
2011-06-22 21:57:30 -0700

    Commented out CEDET (installed Emacs 24 on home desktop, where CEDET comes standard) and included some keybindings for eclim

---
+++ config/eclim-config.el
@@ -0,0 +1,4 @@
+(define-key eclim-mode-map "\C-c\C-m" 'eclim-java-complete)
+(define-key eclim-mode-map "\C-c\C-f" 'eclim-java-find-declaration)
+(define-key eclim-mode-map "\C-c\C-r" 'eclim-java-find-references)
+(define-key eclim-mode-map "\C-c\C-i" 'eclim-java-import-missing)
