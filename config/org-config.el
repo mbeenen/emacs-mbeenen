30dae3803df363bc72f93c1d122ee548d00eebfe
Michael Beenen <michael.beenen@gmail.com>
2011-07-14 18:26:29 -0700

    Simplified org config

--- config/org-config.el
+++ config/org-config.el
@@ -1,15 +1,15 @@
-;; Use remember
-(org-remember-insinuate)
+(setq org-default-notes-file "~/git/org/organizer.org")
 
-;; Remember settings
-(setq org-remember-templates
-      '(("Tasks" ?t "* TODO %?\n  %i\n  %a" "~/emacs/org/organizer.org")                      ;; (2)
-        ("Appointments" ?a "* Appointment: %?\n%^T\n%i\n  %a" "~/emacs/org/organizer.org")))
-(setq remember-annotation-functions '(org-remember-annotation))
-(setq remember-handler-functions '(org-remember-handler))
-(eval-after-load 'remember
-  '(add-hook 'remember-mode-hook 'org-remember-apply-template))
-(global-set-key (kbd "C-c r") 'remember)
+;; I use C-M-r to start capture mode
+(global-set-key (kbd "C-M-r") 'org-capture)
+
+;; Capture templates for: TODO tasks, Notes
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/git/org/organizer.org")
+               "* TODO %?\n%U\n%a\n  %i" )
+              ("n" "note" entry (file "~/git/org/organizer.org")
+               "* %? :NOTE:\n%U\n%a\n  %i" ))))
+
 
 ;; Org settings
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
@@ -22,67 +22,5 @@
 (setq org-agenda-include-diary t)                                                ;; (7)
 (setq org-agenda-include-all-todo t)                                             ;; (8)
 
-(setq org-agenda-files (list "~/emacs/org/organizer.org"))
+(setq org-agenda-files (list "~/git/org/organizer.org"))
 
-;; Clock in/out additional support
-(eval-after-load 'org
-  '(progn
-     (defun wicked/org-clock-in-if-starting ()
-       "Clock in when the task is marked STARTED."
-       (when (and (string= state "STARTED")
-		  (not (string= last-state state)))
-	 (org-clock-in)))
-     (add-hook 'org-after-todo-state-change-hook
-	       'wicked/org-clock-in-if-starting)
-     (defadvice org-clock-in (after wicked activate)
-      "Set this task's status to 'STARTED'."
-      (org-todo "STARTED"))
-    (defun wicked/org-clock-out-if-waiting ()
-      "Clock out when the task is marked WAITING."
-      (when (and (string= state "WAITING")
-                 (equal (marker-buffer org-clock-marker) (current-buffer))
-                 (< (point) org-clock-marker)
-	         (> (save-excursion (outline-next-heading) (point))
-		    org-clock-marker)
-		 (not (string= last-state state)))
-	(org-clock-out)))
-    (add-hook 'org-after-todo-state-change-hook
-	      'wicked/org-clock-out-if-waiting)))
-
-;; Report time for a range 
-(defun org-dblock-write:rangereport (params)
-  "Display day-by-day time reports."
-  (let* ((ts (plist-get params :tstart))
-         (te (plist-get params :tend))
-         (start (time-to-seconds
-                 (apply 'encode-time (org-parse-time-string ts))))
-         (end (time-to-seconds
-               (apply 'encode-time (org-parse-time-string te))))
-         day-numbers)
-    (setq params (plist-put params :tstart nil))
-    (setq params (plist-put params :end nil))
-    (while (<= start end)
-      (save-excursion
-        (insert "\n\n"
-                (format-time-string (car org-time-stamp-formats)
-                                    (seconds-to-time start))
-                "----------------\n")
-        (org-dblock-write:clocktable
-         (plist-put
-          (plist-put
-           params
-           :tstart
-           (format-time-string (car org-time-stamp-formats)
-                               (seconds-to-time start)))
-          :tend
-          (format-time-string (car org-time-stamp-formats)
-                              (seconds-to-time end))))
-        (setq start (+ 86400 start))))))
-
-;; Mark an entry as done once all subentries are marked as done (only if parent item is tracking statistics)
-(defun org-summary-todo (n-done n-not-done)
-       "Switch entry to DONE when all subentries are done, to TODO otherwise."
-       (let (org-log-done org-log-states)   ; turn off logging
-         (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-     
-     (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
