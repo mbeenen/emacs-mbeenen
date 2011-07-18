5d3b2b0af5ab7e96e3301416838585d7ef68f4bf
Michael Beenen <michael.beenen@gmail.com>
2011-07-18 06:50:44 -0700

    Some more setup for org-config

--- config/org-config.el
+++ config/org-config.el
@@ -1,26 +1,52 @@
 (setq org-default-notes-file "~/git/org/organizer.org")
 
-;; I use C-M-r to start capture mode
-(global-set-key (kbd "C-M-r") 'org-capture)
-
 ;; Capture templates for: TODO tasks, Notes
 (setq org-capture-templates
       (quote (("t" "todo" entry (file "~/git/org/organizer.org")
-               "* TODO %?\n%U\n%a\n  %i" )
+               "* TODO %?\n%a\n  %i" )
               ("n" "note" entry (file "~/git/org/organizer.org")
                "* %? :NOTE:\n%U\n%a\n  %i" ))))
 
 
 ;; Org settings
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(setq org-log-done t)
+(setq org-agenda-include-all-todo t)     
+(setq org-agenda-files (list "~/git/org/organizer.org"))
+
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)"))))
+(setq org-use-fast-todo-selection t)
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("SOMEDAY" ("WAITING" . t))
+              (done ("WAITING"))
+              ("TODO" ("WAITING") ("CANCELLED"))
+              ("NEXT" ("WAITING"))
+              ("STARTED" ("WAITING"))
+              ("DONE" ("WAITING") ("CANCELLED")))))
+
+
+;; Custom Key Bindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-(setq org-log-done t)
-(setq org-todo-keywords '("TODO(t)" "STARTED(s)" "VERIFY(v)" "|" "DONE(d)" "DELEGATED(g)" "CANCELLED(c)"))                    ;; (6)
-(setq org-agenda-include-diary t)                                                ;; (7)
-(setq org-agenda-include-all-todo t)                                             ;; (8)
+(global-set-key (kbd "<f12>") 'org-agenda)
+(global-set-key (kbd "<f5>") 'bh/org-todo)
+(global-set-key (kbd "<S-f5>") 'bh/widen)
+(global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
+(global-set-key (kbd "C-M-r") 'org-capture)
 
-(setq org-agenda-files (list "~/git/org/organizer.org"))
+(defun bh/org-todo ()
+  (interactive)
+  (org-narrow-to-subtree)
+  (org-show-todo-tree nil))
 
+(defun bh/widen ()
+  (interactive)
+  (widen)
+  (org-reveal))
