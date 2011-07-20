0f015ab7ded55df15b3adbdaabf729a88902977d
Michael Beenen <michael.beenen@gmail.com>
2011-07-19 20:30:24 -0700

    Shuffled around the order of some settings

--- config/org-config.el
+++ config/org-config.el
@@ -1,4 +1,11 @@
+(require 'org-install)
+
+;; Org settings
+(setq org-agenda-files (list "~/git/org/organizer.org"))
 (setq org-default-notes-file "~/git/org/organizer.org")
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(setq org-log-done t)
+(setq org-agenda-include-all-todo t)     
 
 ;; Capture templates for: TODO tasks, Notes
 (setq org-capture-templates
@@ -6,13 +13,6 @@
                "* TODO %?\n%a\n  %i" )
               ("n" "note" entry (file "~/git/org/organizer.org")
                "* %? :NOTE:\n%U\n%a\n  %i" ))))
-
-
-;; Org settings
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(setq org-log-done t)
-(setq org-agenda-include-all-todo t)     
-(setq org-agenda-files (list "~/git/org/organizer.org"))
 
 (setq org-todo-keywords
       (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
@@ -46,10 +46,8 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-(global-set-key (kbd "<f12>") 'org-agenda)
 (define-key org-mode-map "\C-ct" 'bh/org-todo)
 (define-key org-mode-map "\C-u\C-ct" 'bh/widen)
-(global-set-key (kbd "<f8>") 'org-cycle-agenda-files)
 (global-set-key (kbd "C-M-r") 'org-capture)
 
 (defun bh/org-todo ()
@@ -62,3 +60,5 @@
   (widen)
   (org-reveal))
 
+;; Synchronization related stuffs
+(run-at-time "00:59" 3600 'org-save-all-org-buffers)
