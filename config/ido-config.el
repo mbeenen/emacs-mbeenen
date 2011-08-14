41c48a8c06d0643b3cbfacf7a9cbabcee1de71b7
Michael Beenen <michael.beenen@gmail.com>
2011-08-13 22:16:42 -0700

    * config/ido-config.el: Enable SMEX (ido for M-x) and ido-completing-read just about everywhere

--- config/ido-config.el
+++ config/ido-config.el
@@ -51,6 +51,32 @@
 
 (global-set-key (kbd "C-.") 'ido-find-file-in-tag-files)
 
+(defvar ido-enable-replace-completing-read t
+  "If t, use ido-completing-read instead of completing-read if possible.
+    
+    Set it to nil using let in around-advice for functions where the
+    original completing-read is required.  For example, if a function
+    foo absolutely must use the original completing-read, define some
+    advice like this:
+    
+    (defadvice foo (around original-completing-read-only activate)
+      (let (ido-enable-replace-completing-read) ad-do-it))")
+
+;; Replace completing-read wherever possible, unless directed otherwise
+(defadvice completing-read
+  (around use-ido-when-possible activate)
+  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+          (and (boundp 'ido-cur-list)
+               ido-cur-list)) ; Avoid infinite loop from ido calling this
+      ad-do-it
+    (let ((allcomp (all-completions "" collection predicate)))
+      (if allcomp
+          (setq ad-return-value
+                (ido-completing-read prompt
+                                     allcomp
+                                     nil require-match initial-input hist def))
+        ad-do-it))))
+
 ;; ibuffer sorting
 (setq ibuffer-saved-filter-groups
       (quote (("default"
@@ -82,3 +108,10 @@
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
 
+;; SMEX
+(add-to-list 'load-path "~/.emacs.d/smex")
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x. (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
