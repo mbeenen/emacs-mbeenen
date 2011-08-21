d8878fa30be51a03244cd54ef0d4b199ec528277
Michael Beenen <michael.beenen@gmail.com>
2011-08-21 13:18:21 -0700

    Basic setup for java-config after reading cc-mode docs

--- config/java-config.el
+++ config/java-config.el
@@ -1,5 +1,13 @@
 (defun mbeenen-java-mode-setup ()
   (interactive)
-  (c-toggle-auto-newline t))
+  (c-toggle-auto-newline t)
+  (setq c-hungry-delete-key t)
+  (add-to-list 'c-cleanup-list 'brace-else-brace)
+  (add-to-list 'c-cleanup-list 'brace-elseif-brace)
+  (add-to-list 'c-cleanup-list 'brace-catch-brace)
+  (add-to-list 'c-cleanup-list 'empty-defun-braces)
+  (add-to-list 'c-cleanup-list 'comment-close-slash)
+)
 
 (setq java-mode-hook (quote (mbeenen-java-mode-setup)))
+
