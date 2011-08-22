f2cf2980641135e10617c65037dee7de1ebd185b
Michael Beenen <michael.beenen@gmail.com>
2011-08-21 18:37:29 -0700

    Added some java indentation rules, cleaned up the java-config a bit

--- config/java-config.el
+++ config/java-config.el
@@ -1,15 +1,24 @@
 (defun mbeenen-java-mode-setup ()
-  (interactive)
+  ;; Minor modes
   (c-toggle-auto-newline t)
   (setq c-hungry-delete-key t)
+
+  ;; Indentation cleanup
   (add-to-list 'c-cleanup-list 'brace-else-brace)
   (add-to-list 'c-cleanup-list 'brace-elseif-brace)
   (add-to-list 'c-cleanup-list 'brace-catch-brace)
   (add-to-list 'c-cleanup-list 'empty-defun-braces)
   (add-to-list 'c-cleanup-list 'comment-close-slash)
+  
+  ;; Indentation setup
+  (c-set-offset 'arglist-close 'c-lineup-close-paren)
+  (c-set-offset 'arglist-intro '+)
+
+  ;; Font face customization
+  (set-face-attribute c-annotation-face nil
+                    :foreground "#8cd0d3")
+
 )
 
 (setq java-mode-hook (quote (mbeenen-java-mode-setup)))
 
-(set-face-attribute c-annotation-face nil
-                    :foreground "#8cd0d3")
