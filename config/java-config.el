fd60b9e597806d711a2cf408ded605c9eb409566
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-08-22 18:17:27 -0700

    Somewhat more robust java annotation indentation setup

--- config/java-config.el
+++ config/java-config.el
@@ -1,3 +1,5 @@
+(require 'java-mode-indent-annotations)
+
 (defun mbeenen-java-mode-setup ()
   (interactive)
   ;; Minor modes
@@ -10,17 +12,20 @@
   (add-to-list 'c-cleanup-list 'brace-catch-brace)
   (add-to-list 'c-cleanup-list 'empty-defun-braces)
   (add-to-list 'c-cleanup-list 'comment-close-slash)
-  
+
   ;; Indentation setup
   (c-set-offset 'arglist-close 'c-lineup-close-paren)
   (c-set-offset 'arglist-intro '+)
 
-  (setq c-comment-start-regexp "(@|/(/|[*][*]?))")
-  (modify-syntax-entry ?@ "< b" java-mode-syntax-table)
+  (setq tab-width 4)
+
+  ;; (setq c-comment-start-regexp "(@|/(/|[*][*]?))")
+  ;; (modify-syntax-entry ?@ "< b" java-mode-syntax-table)
   ;; Font face customization
   ;; (set-face-attribute c-annotation-face nil
   ;;                   :foreground "#8cd0d3")
 
+  (java-mode-indent-annotations-setup)
   (eclim-mode t)
 )
 
