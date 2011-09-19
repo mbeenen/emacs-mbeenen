499ee3e82ea149e1d26ab9f44b249ee6cba79195
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-09-18 18:04:51 -0700

    Changed ido-find-file-in-tag-files to handle duplicate file names better, also disable replacement of completing-read everywhere (due to some issues with it and tramp)

--- config/ido-config.el
+++ config/ido-config.el
@@ -33,6 +33,9 @@
 ;;        "Project file: " (tags-table-files) nil t)))))
 
 (defun ido-find-file-in-tag-files ()
+  "Uses the current tags table to construct a list of files in the project, and use ido-completing-read
+to present the choices. Handles duplicate file names by appending the name of the first different directory
+name for the two files, moving up the directory tree step by step."
   (interactive)
   (tags-reset-tags-tables)
   (tags-completion-table)
@@ -42,12 +45,49 @@
     (setq tbl (make-hash-table :test 'equal))
     (let (ido-list)
       (mapc (lambda (path)
-	      ;; format path for display in ido list
-	      (setq key (replace-regexp-in-string "\\(.*?\\)\\([^/]+\\)/\\([^/]+?\\)$" "\\3:\\2" path))
+	      ;; format path for display in ido list, by default displaying the parent directory of the file
+	      (setq key (replace-regexp-in-string "\\(.*?\\)\\([^/]+\\)/\\([^/]+?\\)$" "\\3" path))
 	      ;; remove trailing | or /
 	      (setq key (replace-regexp-in-string "\\(|\\|/\\)$" "" key))
-	      (puthash key path tbl)
-	      (push key ido-list)
+
+              (if (gethash key tbl)
+                  ;; Key already exists in the table, we need to uniquify them
+                  (let* ((old-path (gethash key tbl))
+                         (old-path-tmp (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\1" old-path))
+                         (new-path-tmp (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\1" path))
+                         unique-name-old
+                         unique-name-new)
+                      (setq unique-name-old
+                            (replace-regexp-in-string "\\(.*?\\)\\([^/]+\\)/\\([^/]+?\\)$" "\\3:" path)
+                            unique-name-new
+                            (replace-regexp-in-string "\\(.*?\\)\\([^/]+\\)/\\([^/]+?\\)$" "\\3:" path))
+                      ;; Keep adding directories to the keys until they differentiate
+                      (while (string-equal unique-name-old unique-name-new)
+                        (setq unique-name-old
+                              (concat
+                               (car (split-string unique-name-old ":"))
+                               ":"
+                               (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\2" old-path-tmp)
+                               )
+                              old-path-tmp
+                              (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\1" old-path-tmp)
+                              unique-name-new
+                              (concat
+                               (car (split-string unique-name-new ":"))
+                               ":"
+                               (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\2" new-path-tmp)
+                               )
+                              new-path-tmp
+                              (replace-regexp-in-string "\\(.*?\\)/\\([^/]+?\\)$" "\\1" new-path-tmp)))
+                      (remhash key tbl)
+                      (puthash unique-name-old old-path tbl)
+                      (puthash unique-name-new path tbl)
+                      (setq ido-list (delete key ido-list))
+                      (push unique-name-old ido-list)
+                      (push unique-name-new ido-list))
+                ;; Key does not exist in the table, free to push it in
+                (puthash key path tbl)
+                (push key ido-list))
 	      )
 	    (tags-table-files)
 	    )
@@ -72,31 +112,31 @@
 
 (global-set-key (kbd "C-.") 'ido-find-file-in-tag-files)
 
-(defvar ido-enable-replace-completing-read t
-  "If t, use ido-completing-read instead of completing-read if possible.
+;; (defvar ido-enable-replace-completing-read nil
+;;   "If t, use ido-completing-read instead of completing-read if possible.
     
-    Set it to nil using let in around-advice for functions where the
-    original completing-read is required.  For example, if a function
-    foo absolutely must use the original completing-read, define some
-    advice like this:
+;;     Set it to nil using let in around-advice for functions where the
+;;     original completing-read is required.  For example, if a function
+;;     foo absolutely must use the original completing-read, define some
+;;     advice like this:
     
-    (defadvice foo (around original-completing-read-only activate)
-      (let (ido-enable-replace-completing-read) ad-do-it))")
+;;     (defadvice foo (around original-completing-read-only activate)
+;;       (let (ido-enable-replace-completing-read) ad-do-it))")
 
-;; Replace completing-read wherever possible, unless directed otherwise
-(defadvice completing-read
-  (around use-ido-when-possible activate)
-  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
-          (and (boundp 'ido-cur-list)
-               ido-cur-list)) ; Avoid infinite loop from ido calling this
-      ad-do-it
-    (let ((allcomp (all-completions "" collection predicate)))
-      (if allcomp
-          (setq ad-return-value
-                (ido-completing-read prompt
-                                     allcomp
-                                     nil require-match initial-input hist def))
-        ad-do-it))))
+;; ;; Replace completing-read wherever possible, unless directed otherwise
+;; (defadvice completing-read
+;;   (around use-ido-when-possible activate)
+;;   (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+;;           (and (boundp 'ido-cur-list)
+;;                ido-cur-list)) ; Avoid infinite loop from ido calling this
+;;       ad-do-it
+;;     (let ((allcomp (all-completions "" collection predicate)))
+;;       (if allcomp
+;;           (setq ad-return-value
+;;                 (ido-completing-read prompt
+;;                                      allcomp
+;;                                      nil require-match initial-input hist def))
+;;         ad-do-it))))
 
 ;; ibuffer sorting
 (setq ibuffer-saved-filter-groups
