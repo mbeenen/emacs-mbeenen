bfee871591abe0c0667651fbd9dce92d9017e1f5
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-11-10 13:58:15 -0800

    Added .js to javascript-mode alist

--- config/javascript-config.el
+++ config/javascript-config.el
@@ -1,4 +1,5 @@
 (setq js-indent-level 2)
 
+(setq auto-mode-alist (cons '(".js$" . javascript-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("javascript.mi" . javascript-mode) auto-mode-alist))
 
