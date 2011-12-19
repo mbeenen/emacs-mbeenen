aafe946ab1adfad324d6a3a5bce701e3dd6e4648
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-12-19 05:29:09 +0000

    Removed work-emacs.emacs Added a hard coded config for zenburn, as color themes are not supported in Emacs 23

--- work-emacs.el
+++ work-emacs.el
@@ -42,38 +42,4 @@
 (require 'mbeenen-work-color-theme)
 (require 'mbeenen-work-init)
 (require 'mbeenen-mumamo)
-;; (push "~/projects/emacs-mbeenen/site-lisp" load-path)
-;; (push "~/projects/emacs-mbeenen/config" load-path)
-;; (push "~/projects/emacs-mbeenen/config/work" load-path)
-;; (push "~/projects/emacs-mbeenen/config/libs" load-path)
-
-;; (load-file "~/projects/emacs-mbeenen/config/work/env-variables.el")
-
-;; (load-file "~/projects/emacs-mbeenen/config/cc-mode-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/color-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/dired-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/eclim-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/efuncs.el")
-;; (load-file "~/projects/emacs-mbeenen/config/lisp-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/erc-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/general-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/general-keybindings.el")
-;; (load-file "~/projects/emacs-mbeenen/config/git-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/ido-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/java-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/javascript-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/org-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/perl-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/shell-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/tags-config.el")
-;; ;; (load-file "~/projects/emacs-mbeenen/config/yasnippet-config.el")
-
-;; (load-file "~/projects/emacs-mbeenen/config/work/amz-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/work/color-config.el")
-;; (load-file "~/projects/emacs-mbeenen/config/work/init-shell.el")
-
-;; ;; Load the desktop last
-;; (load-file "~/projects/emacs-mbeenen/config/desktop-config.el")
-
-
 
