45c07a78df5277a5b0cdf713f6cc6dabf820c70f
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-12-20 17:42:39 +0000

    cc-mode java indentation, auto-newline configurations

--- config/mbeenen-java.el
+++ config/mbeenen-java.el
@@ -1,10 +1,13 @@
 (defun mbeenen-java-mode-setup ()
   (interactive)
   ;; Minor modes
-  (c-toggle-auto-newline -1)
+  (c-toggle-auto-newline 1)
   (setq c-hungry-delete-key t)
 
-  ;; Indentation cleanup
+  ;; Auto-newline criteria changes
+  (add-to-list 'c-hanging-semi&comma-criteria 'c-semi&comma-no-newlines-before-nonblanks)
+
+  ;; Auto-newline cleanup
   (add-to-list 'c-cleanup-list 'brace-else-brace)
   (add-to-list 'c-cleanup-list 'brace-elseif-brace)
   (add-to-list 'c-cleanup-list 'brace-catch-brace)
@@ -12,20 +15,22 @@
   (add-to-list 'c-cleanup-list 'comment-close-slash)
 
   ;; Indentation setup
-  (c-set-offset 'arglist-close 'c-lineup-close-paren)
-  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'arglist-close 0) ;;default is c-lineup-close-paren
+  (c-set-offset 'arglist-intro '++)
   (c-set-offset 'arglist-cont-nonempty '++)
+  (c-set-offset 'inexpr-class 0)
+  (c-set-offset 'substatement-open 0)
+  (c-set-offset 'func-decl-cont 0) ;;default is c-lineup-java-throws, I'm a little concerned about this one
+  (c-set-offset 'case-label '+) ;;default is 0
 
   (setq tab-width 4)
 
   ;; Font face customization
   (set-face-attribute c-annotation-face nil
                     :foreground "#8cd0d3")
-
-  ;; (java-mode-indent-annotations-setup)
-  ;; (eclim-mode t)
 )
 
-(setq java-mode-hook (quote (mbeenen-java-mode-setup)))
+(add-hook 'java-mode-hook 'mbeenen-java-mode-setup)
+(add-hook 'java-mode-hook 'prelude-prog-mode-hook)
 
 (provide 'mbeenen-java)
