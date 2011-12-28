ca25e54d4f1fb83d175b8fa744a2225f31768917
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2011-12-28 16:40:25 +0000

    Remove commented out old indentation setup

--- config/mbeenen-java.el
+++ config/mbeenen-java.el
@@ -77,26 +77,6 @@
   (setq c-hungry-delete-key t)
 
   (c-set-style "PERSONAL")
-  ;; ;; (setq c-hanging-braces-alist set-from-style)
-  ;; (add-to-list 'c-hanging-braces-alist '(topmost-intro after))
-  ;; ;; Auto-newline criteria changes
-  ;; (add-to-list 'c-hanging-semi&comma-criteria 'c-semi&comma-no-newlines-before-nonblanks)
-
-  ;; ;; Auto-newline cleanup
-  ;; (add-to-list 'c-cleanup-list 'brace-else-brace)
-  ;; (add-to-list 'c-cleanup-list 'brace-elseif-brace)
-  ;; (add-to-list 'c-cleanup-list 'brace-catch-brace)
-  ;; (add-to-list 'c-cleanup-list 'empty-defun-braces)
-  ;; (add-to-list 'c-cleanup-list 'comment-close-slash)
-
-  ;; ;; Indentation setup
-  ;; (c-set-offset 'arglist-close 0) ;;default is c-lineup-close-paren
-  ;; (c-set-offset 'arglist-intro '++)
-  ;; (c-set-offset 'arglist-cont-nonempty '++)
-  ;; (c-set-offset 'inexpr-class 0)
-  ;; (c-set-offset 'substatement-open 0)
-  ;; (c-set-offset 'func-decl-cont 0) ;;default is c-lineup-java-throws, I'm a little concerned about this one
-  ;; (c-set-offset 'case-label '+) ;;default is 0
 
   (setq tab-width 4)
 
