0aab6929c16c90412b3c51d167476f3c865216f7
Michael <michael.beenen@gmail.com>
2012-02-05 11:22:10 -0800

    Added js2-mode as a submodule, configuration for it

--- config/mbeenen-javascript.el
+++ config/mbeenen-javascript.el
@@ -1,3 +1,3 @@
 (setq js-indent-level 2)
-
+(autoload 'js2-mode "js2-mode" nil t)
 (provide 'mbeenen-javascript)
--- config/mbeenen-mode-mappings.el
+++ config/mbeenen-mode-mappings.el
@@ -1,5 +1,5 @@
 ;; Javascript
-(add-to-list 'auto-mode-alist '(".js$" . javascript-mode))
+(add-to-list 'auto-mode-alist '(".js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("javascript.mi" . javascript-mode))
 
 ;; org mode
