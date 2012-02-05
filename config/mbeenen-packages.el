df33c17c417ff106636f6d459008e2924d61a824
Michael <michael.beenen@gmail.com>
2012-02-04 22:21:30 -0800

    Updated list of packages to be installed through ELPA

--- config/mbeenen-packages.el
+++ config/mbeenen-packages.el
@@ -3,22 +3,23 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-(defvar prelude-packages
-  '()
+(defvar mbeenen-packages
+  '(color-theme ctags dired-single etags-select etags-table magit magithub paredit smex yasnippet
+                zenburn-theme)
   "A list of packages to ensure are installed at launch.")
 
-(defun prelude-packages-installed-p ()
-  (loop for p in prelude-packages
+(defun mbeenen-packages-installed-p ()
+  (loop for p in mbeenen-packages
         when (not (package-installed-p p)) do (return nil)
         finally (return t)))
 
-(unless (prelude-packages-installed-p)
+(unless (mbeenen-packages-installed-p)
   ;; check for new packages (package versions)
   (message "%s" "Emacs Prelude is now refreshing its package database...")
   (package-refresh-contents)
   (message "%s" " done.")
   ;; install the missing packages
-  (dolist (p prelude-packages)
+  (dolist (p mbeenen-packages)
     (when (not (package-installed-p p))
       (package-install p))))
 
