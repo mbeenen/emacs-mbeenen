016125a95f3aff1c899b7e9bf393d85e0f55fdc0
Michael <michael.beenen@gmail.com>
2012-02-04 19:03:04 -0800

    Fixes for initialization without prelude

---
+++ config/mbeenen-packages.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(defvar prelude-packages
+  '()
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'mbeenen-packages)
---
+++ config/mbeenen-programming.el
@@ -0,0 +1,43 @@
+(defun prelude-local-comment-auto-fill ()
+  (set (make-local-variable 'comment-auto-fill-only-comments) t)
+  (auto-fill-mode t))
+
+(defun prelude-add-watchwords ()
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
+          1 font-lock-warning-face t))))
+
+;; We have a number of turn-on-* functions since it's advised that lambda
+;; functions not go in hooks. Repeatedly evaluating an add-to-list with a
+;; hook value will repeatedly add it since there's no way to ensure
+;; that a lambda doesn't already exist in the list.
+
+(defun prelude-turn-on-whitespace ()
+  (whitespace-mode +1))
+
+(defun prelude-turn-off-whitespace ()
+  (whitespace-mode -1))
+
+(defun prelude-turn-on-abbrev ()
+  (abbrev-mode +1))
+
+(defun prelude-turn-off-abbrev ()
+  (abbrev-mode -1))
+
+;; show the name of the current function definition in the modeline
+(require 'which-func)
+(which-func-mode 1)
+
+(defun mbeenen-prog-mode-hook ()
+  "Default coding hook, useful with any programming language."
+  (prelude-local-comment-auto-fill)
+  (prelude-turn-on-whitespace)
+  (prelude-add-watchwords)
+  ;; keep the whitespace decent all the time
+  (add-hook 'before-save-hook 'whitespace-cleanup nil t))
+
+;; in Emacs 24 programming major modes generally derive
+;; from a common mode named prog-mode
+(add-hook 'prog-mode-hook 'mbeenen-prog-mode-hook)
+
+(provide 'mbeenen-programming)
