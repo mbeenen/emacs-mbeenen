8fb11b73510a66b1907e2235633980b8b6b6de09
Michael Beenen <mbeenen@mbeenen.desktop.amazon.com>
2012-02-09 18:35:33 +0000

    Added my standard hooks for js/js2-mode

--- config/mbeenen-javascript.el
+++ config/mbeenen-javascript.el
@@ -1,3 +1,6 @@
 (setq js-indent-level 2)
 (autoload 'js2-mode "js2-mode" nil t)
+
+(add-hook 'js2-mode-hook 'mbeenen-prog-mode-hook)
+(add-hook 'js-mode-hook 'mbeenen-prog-mode-hook)
 (provide 'mbeenen-javascript)
