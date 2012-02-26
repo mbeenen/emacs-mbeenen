52488fb9a151e2183761fbc8e44d665c186721fa
Michael <michael.beenen@gmail.com>
2012-02-26 11:50:51 -0800

    Helper defuns to compile/run java programs at home

--- config/home/mbeenen-emacs-24-specific.el
+++ config/home/mbeenen-emacs-24-specific.el
@@ -3,6 +3,16 @@
 (electric-indent-mode t)
 (electric-layout-mode t)
 
+(defun mbeenen-home-java-run (prompt)
+  "Execute the current java file"
+  (interactive "P")
+  (let ((args (if prompt
+                   (read-string "CLI args: "))))
+    (shell-command
+     (concat "java -cp \".:/home/michael/projects/thinking-in-java\" "
+             (file-name-nondirectory (file-name-sans-extension (buffer-file-name)))
+             " " args))))
+
 ;; but not for java mode
 (defun mbeenen-home-java-setup ()
 
@@ -10,10 +20,15 @@
   (make-local-variable 'electric-pair-mode)
   (make-local-variable 'electric-indent-mode)
   (make-local-variable 'electric-layout-mode)
+  (make-local-variable 'compile-command)
 
   (setq electric-pair-mode nil)
   (setq electric-indent-mode nil)
   (setq electric-layout-mode nil)
+  (setq compile-command (concat "javac -cp \".:/home/michael/projects/thinking-in-java\" "
+                                (file-name-nondirectory (buffer-file-name))))
+
+  (define-key java-mode-map (kbd "C-q C-j") 'mbeenen-home-java-run)
 
   )
 
