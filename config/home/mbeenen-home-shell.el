d43f73bc2052694406cd7a8dc9687b064897746f
Michael <michael.beenen@gmail.com>
2012-03-25 09:16:41 -0700

    Removed unused file

--- config/home/mbeenen-home-shell.el
+++
@@ -1,5 +0,0 @@
-(init-shell "~" "home")
-(init-shell "~/projects/emacs-mbeenen" "emacs-config")
-(init-shell "~/projects/thinking-in-java" "thinking-in-java")
-
-(provide 'mbeenen-home-shell)
--- config/home/mbeenen-home-startup.el
+++ config/home/mbeenen-home-startup.el
@@ -1,4 +1,5 @@
 (init-shell "~" "home")
 (init-shell "~/projects/emacs-mbeenen" "emacs-config")
+(init-shell "~/projects/thinking-in-java" "thinking-in-java")
 
 (provide 'mbeenen-home-startup)
