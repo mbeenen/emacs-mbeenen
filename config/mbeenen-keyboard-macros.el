7eabc3d8b2d178d5bba6947121c7034f37017c96
Michael Beenen <mbeenen@amazon.com>
2012-06-04 15:54:31 +0000

    Added a keyboard macro for unwinding Java stack traces

--- config/mbeenen-keyboard-macros.el
+++ config/mbeenen-keyboard-macros.el
@@ -1,4 +1,7 @@
+;; These macros aren't very generalized, but are usually just some quick n' dirty shortcuts for frequent operations
 (fset 'perl-expand-hash
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 123 2 return 134217742 return 134217744 6] 0 "%d")) arg)))
+(fset 'unwind-java-stack-trace
+   [?\C-s ?| ?\C-m return ?\C-a])
 
 (provide 'mbeenen-keyboard-macros)
