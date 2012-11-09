5dc2fe07be7d009ceaac01f82e1d1ff6e7e8b177
Michael Beenen <mbeenen@mbeenen-4.desktop.amazon.com>
2012-11-09 16:26:44 +0000

    Revert to using plain javascript-mode, js2-mode was painfully slow on large files

--- config/mbeenen-mode-mappings.el
+++ config/mbeenen-mode-mappings.el
@@ -1,5 +1,5 @@
 ;; Javascript
-(add-to-list 'auto-mode-alist '(".js$" . js2-mode))
+(add-to-list 'auto-mode-alist '(".js$" . javascript-mode))
 
 ;; org mode
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
