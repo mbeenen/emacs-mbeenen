6f0c8157b346d8cdfba0af88d0d84937cd702ede
Michael Beenen <michael.beenen@gmail.com>
2013-02-13 21:23:15 -0800

    Some keybindings for sending js code to the repl

--- config/mbeenen-javascript.el
+++ config/mbeenen-javascript.el
@@ -18,4 +18,12 @@
          (lambda (output)
            (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
 
+(add-hook 'js2-mode-hook '(lambda () 
+			    (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+			    ;; (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+			    ;; (local-set-key "\C-cb" 'js-send-buffer)
+			    ;; (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+			    ;; (local-set-key "\C-cl" 'js-load-file-and-go)
+			    ))
+
 (provide 'mbeenen-javascript)
