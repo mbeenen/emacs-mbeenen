af0a8db51cb7b3d1a74269d07b36bbf3ede2f5a0
Michael Beenen <mbeenen@amazon.com>
2013-07-02 16:13:08 -0700

    Add helper to copy buffer file name

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -22,3 +22,13 @@
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
+
+(defun prelude-copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
