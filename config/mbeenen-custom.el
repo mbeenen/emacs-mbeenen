9d84c957a067677513047d82ba15862039343b31
Michael Beenen <mbeenen@amazon.com>
2013-07-02 16:33:45 -0700

    Add custom tags command for projectile to account for mason files

--- config/mbeenen-custom.el
+++ config/mbeenen-custom.el
@@ -6,13 +6,14 @@
  ;; If there is more than one, they won't work right.
  '(enable-local-variables :all)
  '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
+ '(projectile-tags-command "ctags -Re --langmap=HTML:.m.mi %s %s")
  '(yas/triggers-in-field t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(c-annotation-face ((t (:foreground "#8cd0d3"))) t)
+ '(c-annotation-face ((t (:foreground "#8cd0d3"))))
  '(cperl-array-face ((t (:foreground "#f0dfaf" :weight bold))))
  '(cperl-hash-face ((t (:foreground "#f0dfaf" :slant italic :weight bold))))
  '(mmm-default-submode-face ((t (:background "#4f4f4f"))))
