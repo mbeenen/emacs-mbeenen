c6380a2beaeffafa7510cac2af2583a933cd5570
Michael Beenen <mbeenen@amazon.com>
2013-08-06 08:12:42 -0700

    Add a couple more unwind macros

--- config/mbeenen-keyboard-macros.el
+++ config/mbeenen-keyboard-macros.el
@@ -3,5 +3,9 @@
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 123 2 return 134217742 return 134217744 6] 0 "%d")) arg)))
 (fset 'unwind-java-stack-trace
    [?\C-s ?| ?\C-m return ?\C-a])
+(fset 'unwind-bsf
+   [?\C-\M-s ?: ?\\ ?| ?\{ ?\\ ?| ?\} ?\C-m return])
+(fset 'unwind-timing-log
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 44 13 return] 0 "%d")) arg)))
 
 (provide 'mbeenen-keyboard-macros)
