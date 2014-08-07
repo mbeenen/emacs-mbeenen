ac81ea56fa66220f96a851593efc9ed87860c3a4
Michael Beenen <mbeenen@amazon.com>
2014-08-07 15:07:02 -0700

    Colorize the compilation buffer

--- config/mbeenen-programming.el
+++ config/mbeenen-programming.el
@@ -44,4 +44,12 @@
 ;; Compilation related settings
 (setq compilation-ask-about-save nil)
 
+;; Colorize the compilation buffer
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 (provide 'mbeenen-programming)
