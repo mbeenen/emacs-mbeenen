8ed5baf8e6cc2df8121cb021ccdda2575b9d0763
Michael Beenen <mbeenen@amazon.com>
2015-07-16 15:12:30 +0000

    Remove obselete module css-complete

--- config/mbeenen-css.el
+++ config/mbeenen-css.el
@@ -1,5 +1,7 @@
 (setq css-indent-offset 4)
 
+(add-to-list 'auto-mode-alist '("\\.\\(scss\\|css\\)$" . scss-mode))
+
 ;; Override SCSS trying to compile on save (Brazil takes care of this for me)
 (setq scss-compile-at-save nil)
 
