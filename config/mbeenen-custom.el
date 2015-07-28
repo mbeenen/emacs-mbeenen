3526417307e90c36a523b8bc2c3fd4150d67b1fd
Beenen <mbeenen@amazon.com>
2015-07-28 11:23:25 -0700

    Formatting of mbeenen-custom

--- config/mbeenen-custom.el
+++ config/mbeenen-custom.el
@@ -5,18 +5,30 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(enable-local-variables :all)
- '(js2-basic-offset 2)
+ '(js2-basic-offset 2 t)
  '(js2-bounce-indent-p nil)
  '(js2-include-node-externs t)
  '(js2-strict-inconsistent-return-warning nil)
  '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
- '(mmm-major-mode-preferences (quote ((perl perl-mode) (python python-mode python-mode) (javascript javascript-mode c++-mode) (java jde-mode java-mode c++-mode) (css css-mode c++-mode))))
+ '(mmm-major-mode-preferences
+   (quote
+    ((perl perl-mode)
+     (python python-mode python-mode)
+     (javascript javascript-mode c++-mode)
+     (java jde-mode java-mode c++-mode)
+     (css css-mode c++-mode))))
+ '(package-selected-packages
+   (quote
+    (repository-root rcirc-notify projectile paredit mark-multiple magit js2-mode js-comint ido-ubiquitous grep-a-lot expand-region etags-table etags-select dired-single dired-details+ dired-details ctags color-theme color-moccur auto-complete ag ace-jump-mode)))
  '(perl-indent-continued-arguments 4)
  '(perl-indent-parens-as-block t)
- '(projectile-tags-command "ctags -Re --langmap=HTML:.m.mi..dhandler..autohandler,Perl:.t.pm.pl -f %s %s")
+ '(projectile-tags-command
+   "ctags -Re --langmap=HTML:.m.mi..dhandler..autohandler,Perl:.t.pm.pl -f %s %s")
  '(repository-root-exhaustive-scan t)
  '(repository-root-force-scan nil)
- '(tls-program (quote ("/apollo/env/envImprovement/bin/openssl s_client -connect %h:%p -no_ssl2 -ign_eof")))
+ '(tls-program
+   (quote
+    ("/apollo/env/envImprovement/bin/openssl s_client -connect %h:%p -no_ssl2 -ign_eof")))
  '(yas/triggers-in-field t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
