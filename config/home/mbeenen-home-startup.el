01375f1bd7b160fada56fc61d8ff54c8014110b5
Michael <michael.beenen@gmail.com>
2015-10-03 20:22:36 -0700

    Update home shells

---
+++ config/home/mbeenen-home-startup.el
@@ -0,0 +1,6 @@
+(init-shell "~" "~")
+(init-shell "~/projects/emacs-mbeenen" "elisp")
+(init-shell "~/git/node-sandbox" "node sandbox")
+(init-shell "~/git/hearthstone-analytics" "hs app")
+
+(provide 'mbeenen-home-startup)
