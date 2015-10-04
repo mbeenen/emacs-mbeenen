fca2a233ff892c8fe28c2059d052b942e42049ce
Michael <michael.beenen@gmail.com>
2015-10-03 20:22:17 -0700

    Fix marmalade repo setup?

--- config/mbeenen-packages.el
+++ config/mbeenen-packages.el
@@ -1,6 +1,6 @@
 (require 'package)
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
@@ -32,7 +32,7 @@
     paredit
     projectile
     rcirc-notify
-    ; repository-root
+    repository-root
     rvm
     s
     scss-mode
